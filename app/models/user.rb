c774c8bb076a10bc5cc7669284c04ca2e8660099
Sxca Ush <nikitos.21333@gmail.com>
2025-02-13 15:43:30 +0300

    Add roles admin n user and upd index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  ROLES = %w[user admin]
+
+  def admin?
+    role == "admin"
+    
+  end
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          validates :username, presence: true, uniqueness: true
