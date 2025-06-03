d3269aa7f651cb832691f7bb7cbf0f3c4836b29e
destinyhover <nikitos.21333@gmail.com>
2025-06-03 21:47:59 +0300

    upd

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   validates :author, presence: true
   validates :body, presence: true
-  validates :body, length: { in: 10..4000 }
+  validates :body, length: { in: 3..4000 }
 
   belongs_to :article
 end
